e963ba59b44ea92dd1ac3c8bcee2c0ceaba905cc
Helen-Sen <senchikhina.elena@gmail.com>
2024-09-21 01:17:08 +0300

    registered user flow v2

--- test/pageobjects/wheel_page.js
+++ test/pageobjects/wheel_page.js
@@ -5,6 +5,44 @@ class WheelPage extends Page {
   open() {
     return super.open(this.path);
   }
-};
+
+  get myWheelResultsTitle() {
+    return $('//h4[text()="My wheel results"]');
+  }
+  get editMyWheelResultsButton() {
+    return $('//button[@type="button" and span[text()="edit my wheel results"]]');
+  }
+
+  get balanceWheel() {
+    return $('//div[@class="_root-wheel-container_1qdfz_6"]/canvas');
+  }
+  get editMyWheelResultsTitle() {
+    return $('//h4[text()="Edit my wheel results"]');
+  }
+
+  get editManuallyButton() {
+    return $('//button[@type="button" and span[text()="Edit manually"]]');
+  }
+
+  get retakeQuizButton() {
+    return $('//button[@type="button" and span[text()="Retake quiz"]]');
+  }
+
+  get saveChangesButton() {
+    return $('//button[@type="button" and span[text()="Save changes"]]');
+  }
+
+  get slidersTitle() {
+    return $('//p[text()="Do you feel any changes in anything? Estimate the fields from 1 to 10"]');
+  }
+
+  get sliders() {
+    return $$('//div[@class="_scores-container_rw7pp_20"]//input[@id="slider"]');
+  }
+
+  get categories() {
+    return $$('//div[@class="_categories-container_1dmv3_12"]//div/label');
+  }
+}
 
 module.exports = new WheelPage();
--- test/specs/registered_user_flow.js
+++ test/specs/registered_user_flow.js
@@ -1,9 +1,10 @@
+const assert = require("assert");
 const signInPage = require("../pageobjects/sign-in_page.js");
 const wheelPage = require("../pageobjects/wheel_page.js");
 const myTasksPage = require("../pageobjects/my_tasks_page.js");
 
-describe("login tests", () => {
-  it("Test: Check Login", async () => {
+describe("Registered user flow", () => {
+  it("Test: Check user login and redirected to 'My wheel results' page", async () => {
     // Open the sign in page
     await signInPage.open();
     console.log("Sign in page URL:", signInPage.getPageUrl());
@@ -20,10 +21,58 @@ describe("login tests", () => {
 
     // Check the user on the wheel page
     await expect(browser).toHaveUrl(wheelPage.getPageUrl());
+    await browser.pause(3000);
 
-    // Go to my task page
-    await signInPage.myTasksButton.click();
+    // Check the page title "My wheel results" is present and visible
+
+    // assert.strictEqual(await wheelPage.myWheelResultsTitle.isExisting(), true, "Title 'My wheel results' is not present on the page");
+    assert.strictEqual(await wheelPage.myWheelResultsTitle.isDisplayed(), true, "Title 'My wheel results' is present but not visible on the page");
+
+    // Check the Wheel is present and visible
+    // assert.strictEqual(await wheelPage.balanceWheel.isExisting(), true, "Balance Wheel is not present on the page");
+    assert.strictEqual(await wheelPage.balanceWheel.isDisplayed(), true, "Balance Wheel is present but not visible on the page");
+
+    // // Go to my task page
+    // await signInPage.myTasksButton.click();
+    // await browser.pause(1000);
+    // await expect(browser).toHaveUrl(myTasksPage.getPageUrl());
+  });
+
+  it("Test: Check user can edit 'My wheel results'", async () => {
+    // Click the Edit my wheel result button
+    await wheelPage.editMyWheelResultsButton.click();
     await browser.pause(1000);
-    await expect(browser).toHaveUrl(myTasksPage.getPageUrl());
+
+    // Check the page title "Edit my wheel results" is present and visible
+
+    // assert.strictEqual(await wheelPage.editMyWheelResultsTitle.isExisting(), true, "Title 'Edit my wheel results' is not present on the page");
+    assert.strictEqual(await wheelPage.editMyWheelResultsTitle.isDisplayed(), true, "Title 'Edit my wheel results' is present but not visible on the page");
+
+    // Check the "Edit manually" and "Retake quiz" buttons are present and visible
+
+    // assert.strictEqual(await wheelPage.editManuallyButton.isExisting(), true, "'Edit my wheel results' button is not present on the page");
+    assert.strictEqual(await wheelPage.editManuallyButton.isDisplayed(), true, "'Edit my wheel results' button is present but not visible on the page");
+
+    // assert.strictEqual(await wheelPage.retakeQuizButton.isExisting(), true, "'Retake quiz' button is not present on the page");
+    assert.strictEqual(await wheelPage.retakeQuizButton.isDisplayed(), true, "'Retake quiz' button is present but not visible on the page");
+
+    // Check the sliders are present and visible
+    // assert.strictEqual(await wheelPage.slidersTitle.isExisting(), true, "Sliders title is not present on the page");
+    assert.strictEqual(await wheelPage.slidersTitle.isDisplayed(), true, "Sliders title button is present but not visible on the page");
+
+    // Log the number of input elements found
+    console.log("Number of sliders:", await wheelPage.sliders.length);
+
+    assert.strictEqual(await wheelPage.sliders.length, 8, "There are not exactly 8 slider elements on the page");
+
+    await wheelPage.saveChangesButton.click();
+    await browser.pause(1000);
+
+    await wheelPage.editMyWheelResultsButton.click();
+
+    await wheelPage.retakeQuizButton.click();
+     await browser.pause(1000);
+
+    console.log("Number of categories:", await wheelPage.categories.length);
   });
 });
